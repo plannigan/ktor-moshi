71496142a2e5c3910786c5a30d769b88fa63459b
Patrick Lannigan <p.lannigan@gmail.com>
2023-03-13 10:50:27 -0400

    Use version catalog for dependencies

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,10 +1,1 @@
 include("core", "sample")
-
-pluginManagement {
-    plugins {
-        kotlin("jvm") version "1.7.10"
-        kotlin("kapt") version "1.7.10"
-        id("com.jfrog.bintray") version "1.8.5"
-        id("com.github.johnrengelman.shadow") version "7.1.2"
-    }
-}
